e0f5b0b9d36581626a171b6029eaaa3697092dca
phoenix-da-phenom <ehinormichael@ymail.com>
2026-01-24 22:45:07 +0100

    input added

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <div>
+    <input type="text" />
+    <input type= "password"/>
+    <button type="submit"/>
+        </div>
+
+
+    </div>
   )
 }
