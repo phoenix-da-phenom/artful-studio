4bac30a61d09f0f50133ed1eed0f1106e2e2296d
phoenix-da-phenom <ehinormichael@ymail.com>
2026-01-27 21:57:08 +0100

    Added login input field

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -7,12 +7,18 @@ export default async function page() {
         },1200)
     })
   return (
-    <div>
-      <div>
-        <input type="text" />
-        <input type="password" />
-        <button type="submit" />
+    <div className="w-[450px]">
+    <div className="border border-gray-400 rounded-md shadow-2xl  p-6" >
+     <h4 className="text-3xl font-semibold text-center mb-4">Login</h4>
+     
+      <div className="flex flex-col gap-4">
+        <input className="border p-2 border-gray-400 rounded-md w-full" type="text" />
+        <input className="border p-2 border-gray-400 rounded-md w-full" type="password" />
+        <button className="bg-primary p-2 font-semibold text-foreground hover:bg-primary/25" type="submit" >login</button>
       </div>
     </div>
+
+    </div>
+
   );
 }
