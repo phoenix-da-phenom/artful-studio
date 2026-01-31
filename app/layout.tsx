6b755daf9e493d064edb4fb450e07f30bc563e81
phoenix-da-phenom <ehinormichael@ymail.com>
2026-01-31 14:49:03 +0100

    layout aadujusted

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal:React.ReactNode
 }>) {
   return (
     <html lang="en">
@@ -31,6 +33,7 @@ export default function RootLayout({
         <div className="py-2 bg-background/30 min-h-screen">
           <Navbar />
           {children}
+          {modal}
         </div>
       </body>
     </html>
