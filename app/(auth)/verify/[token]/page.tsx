4b6fc91efda539d13be1e49fbd24db4590b8f12b
phoenix-da-phenom <ehinormichael@ymail.com>
2026-02-27 18:24:03 +0100

    Worked on verification route

---
+++ app/(auth)/verify/[token]/page.tsx
@@ -0,0 +1,30 @@
+import {API} from "@/util/apiDir"
+export default async function Page({
+  params,
+}:{params:{token: string}}){
+const {token}= params
+try{
+  const res = await fetch(`${API.BASE_URL}/api/v1/auth/verify`,{
+    method:"POST",
+    headers:{
+      "Content-Type": "application/json",
+    },
+    body:JSON.stringify({token}),
+    cache:"no-cache" //Important for POST in server components
+  });
+
+  if (!res.ok){
+    throw new Error("verification failed")
+  }
+
+  const data = await res.json();
+  console.log(data)
+
+}catch(error){
+
+}
+return<div>Token {token}</div>
+
+
+
+}
