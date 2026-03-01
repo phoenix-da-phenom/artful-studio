939df0ba3ef16be83dfffe3abbb8108329309a18
phoenix-da-phenom <ehinormichael@ymail.com>
2026-03-01 09:23:39 +0100

    fixed typo

--- app/Components/WhatMakesThisGalleryDifferent.tsx
+++ app/Components/WhatMakesThisGalleryDifferent.tsx
@@ -5,7 +5,7 @@ export default function WhatMakesThisGalleryDifferent() {
     <div className="">
         <div className="flex flex-col gap-6 items-center">
             <h4 className="font-bold">Essentials</h4>
-            <h1 className="text-4xl font-semibold">What makes this gallery different </h1>
+            <h1 className="text-4xl font-semibold text-center">What makes this gallery different </h1>
             <p>We built this place for photographers and views alike. No clutter, no nonsense, just images that speak for themselves.</p>
 
         </div>
